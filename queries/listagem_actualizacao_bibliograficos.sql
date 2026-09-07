99516c1c1d8df8ddbf55fe78db5d6c152157a8e9
Rui Francisco <blackmolly@gmail.com>
2026-09-07 14:20:32 +0100

    updates to the functions

--- queries/listagem_actualizacao_bibliograficos.sql
+++ queries/listagem_actualizacao_bibliograficos.sql
@@ -1,34 +1,34 @@
 --metadb:function listagem_actualizacao_bibliograficos
 
-DROP FUNCTION IF EXISTS listagem_actualizacao_bibliograficos;
+DROP FUNCTION IF EXISTS listagem_actualizacao_bibliograficos(date, date);
 
 CREATE FUNCTION listagem_actualizacao_bibliograficos(
     start_date date DEFAULT '2020-01-01',
-    end_date date DEFAULT '2050-01-01'
+    end_date   date DEFAULT '2050-01-01'
 )
 RETURNS TABLE(
-    hrid,
-    titulo,
-    data_actualizacao,
-    timestamp_actualizacao,
-    IDUsuarioCriador,
-    CodigoUsuario,
-    NomeUsuario,
-    GrupoUsuario
-    )
+    hrid                    text,
+    titulo                  text,
+    data_actualizacao       text,
+    timestamp_actualizacao  timestamptz,
+    idusuariocriador        uuid,
+    codigousuario           text,
+    nomeusuario             text,
+    grupousuario            text
+)
 AS $$
 
 SELECT
-    i.jsonb->>'hrid'  AS hrid,
-    i.jsonb->>'title' AS title,
-    to_char((i.jsonb->'metadata'->>'updatedDate')::date, 'YYYY-MM-DD') AS data_actualizacao,
-    (i.jsonb->'metadata'->>'updatedDate')::timestamptz AS timestamp_actualizacao,
-    u.id AS userid,
-    u.jsonb->>'username' AS username,
+    i.jsonb->>'hrid',
+    i.jsonb->>'title',
+    to_char((i.jsonb->'metadata'->>'updatedDate')::date, 'YYYY-MM-DD'),
+    (i.jsonb->'metadata'->>'updatedDate')::timestamptz,
+    u.id,
+    u.jsonb->>'username',
     concat_ws(' ',
         u.jsonb->'personal'->>'firstName',
-        u.jsonb->'personal'->>'lastName') AS usuario,
-    g.desc AS GrupoUsuario
+        u.jsonb->'personal'->>'lastName'),
+    g.desc
 FROM folio_inventory.instance__ i
 LEFT JOIN folio_users.users__ u
        ON u.id = (i.jsonb->'metadata'->>'updatedByUserId')::uuid
@@ -38,10 +38,13 @@ LEFT JOIN folio_users.groups__t__ g
       AND g.__current
 WHERE (i.jsonb->'metadata'->>'updatedDate')::date BETWEEN start_date AND end_date
   AND i.__current
-ORDER BY timestamp_actualizacao DESC, usuario
+ORDER BY
+    (i.jsonb->'metadata'->>'updatedDate')::timestamptz DESC,
+    concat_ws(' ',
+        u.jsonb->'personal'->>'firstName',
+        u.jsonb->'personal'->>'lastName')
 
 $$
 LANGUAGE SQL
 STABLE
 PARALLEL SAFE;
-
--- queries/listagem_novos_bibliograficos.sql
+++ queries/listagem_novos_bibliograficos.sql
@@ -1,34 +1,34 @@
 --metadb:function listagem_novos_bibliograficos
 
-DROP FUNCTION IF EXISTS listagem_novos_bibliograficos;
+DROP FUNCTION IF EXISTS listagem_novos_bibliograficos(date, date);
 
 CREATE FUNCTION listagem_novos_bibliograficos(
     start_date date DEFAULT '2020-01-01',
-    end_date date DEFAULT '2050-01-01'
+    end_date   date DEFAULT '2050-01-01'
 )
 RETURNS TABLE(
-    hrid,
-    titulo,
-    data_criacao,
-    timestamp_criacao,
-    IDUsuarioCriador,
-    CodigoUsuario,
-    NomeUsuario,
-    GrupoUsuario
-    )
+    hrid                text,
+    titulo              text,
+    data_criacao        text,
+    timestamp_criacao   timestamptz,
+    idusuariocriador    uuid,
+    codigousuario       text,
+    nomeusuario         text,
+    grupousuario        text
+)
 AS $$
 
 SELECT
-    i.jsonb->>'hrid'  AS hrid,
-    i.jsonb->>'title' AS title,
-    to_char((i.jsonb->'metadata'->>'createdDate')::date, 'YYYY-MM-DD') AS data_criacao,
-    (i.jsonb->'metadata'->>'createdDate')::timestamptz AS timestamp_criacao,
-    u.id AS userid,
-    u.jsonb->>'username' AS username,
+    i.jsonb->>'hrid',
+    i.jsonb->>'title',
+    to_char((i.jsonb->'metadata'->>'createdDate')::date, 'YYYY-MM-DD'),
+    (i.jsonb->'metadata'->>'createdDate')::timestamptz,
+    u.id,
+    u.jsonb->>'username',
     concat_ws(' ',
         u.jsonb->'personal'->>'firstName',
-        u.jsonb->'personal'->>'lastName') AS usuario,
-    g.desc AS GrupoUsuario
+        u.jsonb->'personal'->>'lastName'),
+    g.desc
 FROM folio_inventory.instance__ i
 LEFT JOIN folio_users.users__ u
        ON u.id = (i.jsonb->'metadata'->>'createdByUserId')::uuid
@@ -38,11 +38,13 @@ LEFT JOIN folio_users.groups__t__ g
       AND g.__current
 WHERE (i.jsonb->'metadata'->>'createdDate')::date BETWEEN start_date AND end_date
   AND i.__current
-ORDER BY timestamp_criacao DESC, usuario
+ORDER BY
+    (i.jsonb->'metadata'->>'createdDate')::timestamptz DESC,
+    concat_ws(' ',
+        u.jsonb->'personal'->>'firstName',
+        u.jsonb->'personal'->>'lastName')
 
 $$
 LANGUAGE SQL
 STABLE
 PARALLEL SAFE;
-
-
